8d2677271eadec553e5a496b69dbacd0b6cf03ef
Matthias Kretschmer <matthias@aircloak.com>
2015-06-02 09:21:04 +0200

    my own vim keymap I prefer

--- app/js/lib/vim_patch.js
+++ app/js/lib/vim_patch.js
@@ -2,11 +2,18 @@ define(function(require, exports, module) {
     // Monkey patch vim mode
     var vimKeyBindings = require("ace/keyboard/vim");
 
+    var keepKeys = ["<C-e>", "<C-p>", "<C-f>", "<C-g>", "<C-[>", "<C-]>"];
+    
+    function shouldKeepKey(key) {
+      var res = _.find(keepKeys, function(keys) { return keys === key.keys; });
+      return res !== undefined;
+    }
+
     function patchVimKeys() {
         var vimKeys = vimKeyBindings.handler.defaultKeymap;
         for(var i = 0; i < vimKeys.length; i++) {
             var key = vimKeys[i];
-            if(key.keys.indexOf("<C-") === 0 || (key.type == "keyToKey" && key.toKeys.indexOf("<C-") === 0)) {
+            if (shouldKeepKey(key)) {
                 vimKeys.splice(i, 1);
                 i--;
             }
